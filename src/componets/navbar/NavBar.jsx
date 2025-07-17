c6f34a177012ddab36597f920bac72dc58e3e9ca
Davidsousaaz <david.sousagit0048@gmail.com>
2025-07-17 15:28:18 -0300

    feat(navigation): Integra CRUD de Coordenadores nas rotas e menu admin

--- src/componets/navbar/NavBar.jsx
+++ src/componets/navbar/NavBar.jsx
@@ -1,6 +1,6 @@
 import { jwtDecode } from "jwt-decode";
-import NavItem from "./NavItem";
-import ScrollContainer from "./ScrollContainer";
+import NavItem from "../../../../../Downloads/projeto-sgm-react-feature-crud_disciplina/projeto-sgm-react-feature-crud_disciplina/src/componets/navbar/NavItem.jsx";
+import ScrollContainer from "../../../../../Downloads/projeto-sgm-react-feature-crud_disciplina/projeto-sgm-react-feature-crud_disciplina/src/componets/navbar/ScrollContainer.jsx";
 
 function NavBar() {
   const token = localStorage.getItem("token");
@@ -17,47 +17,49 @@ function NavBar() {
   }
 
   return (
-    <nav>
-      {/* Exibir item apenas para certos perfis */}
-      {perfil === "monitor" && (
-        <ScrollContainer>
-          <NavItem to="/monitor" label="Monitoria" />
-        </ScrollContainer>
-      )}
+      <nav>
+        {/* Exibir item apenas para certos perfis */}
+        {perfil === "monitor" && (
+            <ScrollContainer>
+              <NavItem to="/monitor" label="Monitoria" />
+            </ScrollContainer>
+        )}
 
-      {perfil === "professor" && (
-        <ScrollContainer>
-          <NavItem to="/professor" label="Painel Professor" />
-        </ScrollContainer>
-      )}
+        {perfil === "professor" && (
+            <ScrollContainer>
+              <NavItem to="/professor" label="Painel Professor" />
+            </ScrollContainer>
+        )}
 
-      {perfil === "aluno" && (
-        <ScrollContainer>
-          {/*<NavItem to='/perfil' label="Perfil" />*/}
-          <NavItem to="/editais" label="Editais" />
-          <NavItem to="/monitorias" label="Agenda" />
-        </ScrollContainer>
-      )}
+        {perfil === "aluno" && (
+            <ScrollContainer>
+              {/*<NavItem to='/perfil' label="Perfil" />*/}
+              <NavItem to="/editais" label="Editais" />
+              <NavItem to="/monitorias" label="Agenda" />
+            </ScrollContainer>
+        )}
 
-      {perfil === "admin" && (
-        <ScrollContainer>
-          <NavItem to="/admin" label="Painel admin" />
-          <NavItem to="/instituicoes" label="Instituições" />
-          <NavItem to="/alunos" label="Alunos" />
-          <NavItem to="/disciplinas" label="Disciplinas" />
-        </ScrollContainer>
-      )}
+        {perfil === "admin" && (
+            <ScrollContainer>
+              <NavItem to="/admin" label="Painel admin" />
+              <NavItem to="/instituicoes" label="Instituições" />
+              {/* LINHA ADICIONADA */}
+              <NavItem to="/coordenadores" label="Coordenadores" />
+              <NavItem to="/alunos" label="Alunos" />
+              <NavItem to="/disciplinas" label="Disciplinas" />
+            </ScrollContainer>
+        )}
 
-      {perfil === "coordenador" && (
-        <ScrollContainer>
-          <NavItem to="/coordenador" label="Coordenação" />
-          <NavItem to="/alunos" label="Alunos" />
-          <NavItem to="/disciplinas" label="Disciplinas" />
-          <NavItem to="/monitor" label="Monitoria" />
-          <NavItem to="/professor" label="Painel Professor" />
-        </ScrollContainer>
-      )}
-    </nav>
+        {perfil === "coordenador" && (
+            <ScrollContainer>
+              <NavItem to="/coordenador" label="Coordenação" />
+              <NavItem to="/alunos" label="Alunos" />
+              <NavItem to="/disciplinas" label="Disciplinas" />
+              <NavItem to="/monitor" label="Monitoria" />
+              <NavItem to="/professor" label="Painel Professor" />
+            </ScrollContainer>
+        )}
+      </nav>
   );
 }
 
--- src/componets/rotas/AppRoutes.jsx
+++ src/componets/rotas/AppRoutes.jsx
@@ -1,186 +1,229 @@
 import { Routes, Route, useLocation } from "react-router-dom";
-import Login from "../pages/Login";
-import Aluno from "../pages/Aluno";
-import Professor from "../pages/Professor";
-import Coordenador from "../pages/Coordenador";
-import Monitor from "../pages/Monitor";
-import Admin from "../pages/Admin";
-import CadastrarAluno from "../pages/CadastroAluno";
-import SenhaEsquecida from "../pages/SenhaEsquecida";
-import RotaProtegida from "./RotaProtegida";
-import Cabeca from "../layout/Conteiner";
-import Perfil from "../pages/Perfil";
-import Editais from "../pages/Editais";
-import Monitorias from "../pages/Monitorias";
-import Alunos from "../pages/Alunos";
-import Instituicoes from "../pages/Instituicoes";
-import NovaInstituicao from "../pages/NovaInstituicao";
-import EditarInstituicao from "../pages/EditarInstituicao";
-import Disciplinas from "../pages/Disciplinas";
-import NovaDisciplina from "../pages/NovaDisciplina";
-import EditarDisciplina from "../pages/EditarDisciplina";
+import Login from "../../../../../Downloads/projeto-sgm-react-feature-crud_disciplina/projeto-sgm-react-feature-crud_disciplina/src/componets/pages/Login.jsx";
+import Aluno from "../../../../../Downloads/projeto-sgm-react-feature-crud_disciplina/projeto-sgm-react-feature-crud_disciplina/src/componets/pages/Aluno.jsx";
+import Professor from "../../../../../Downloads/projeto-sgm-react-feature-crud_disciplina/projeto-sgm-react-feature-crud_disciplina/src/componets/pages/Professor.jsx";
+import Coordenador from "../../../../../Downloads/projeto-sgm-react-feature-crud_disciplina/projeto-sgm-react-feature-crud_disciplina/src/componets/pages/Coordenador.jsx";
+import Monitor from "../../../../../Downloads/projeto-sgm-react-feature-crud_disciplina/projeto-sgm-react-feature-crud_disciplina/src/componets/pages/Monitor.jsx";
+import Admin from "../../../../../Downloads/projeto-sgm-react-feature-crud_disciplina/projeto-sgm-react-feature-crud_disciplina/src/componets/pages/Admin.jsx";
+import CadastrarAluno from "../../../../../Downloads/projeto-sgm-react-feature-crud_disciplina/projeto-sgm-react-feature-crud_disciplina/src/componets/pages/CadastroAluno.jsx";
+import SenhaEsquecida from "../../../../../Downloads/projeto-sgm-react-feature-crud_disciplina/projeto-sgm-react-feature-crud_disciplina/src/componets/pages/SenhaEsquecida.jsx";
+import RotaProtegida from "../../../../../Downloads/projeto-sgm-react-feature-crud_disciplina/projeto-sgm-react-feature-crud_disciplina/src/componets/rotas/RotaProtegida.jsx";
+import Cabeca from "../../../../../Downloads/projeto-sgm-react-feature-crud_disciplina/projeto-sgm-react-feature-crud_disciplina/src/componets/layout/Conteiner.jsx";
+import Perfil from "../../../../../Downloads/projeto-sgm-react-feature-crud_disciplina/projeto-sgm-react-feature-crud_disciplina/src/componets/pages/Perfil.jsx";
+import Editais from "../../../../../Downloads/projeto-sgm-react-feature-crud_disciplina/projeto-sgm-react-feature-crud_disciplina/src/componets/pages/Editais.jsx";
+import Monitorias from "../../../../../Downloads/projeto-sgm-react-feature-crud_disciplina/projeto-sgm-react-feature-crud_disciplina/src/componets/pages/Monitorias.jsx";
+import Alunos from "../../../../../Downloads/projeto-sgm-react-feature-crud_disciplina/projeto-sgm-react-feature-crud_disciplina/src/componets/pages/Alunos.jsx";
+import Instituicoes from "../../../../../Downloads/projeto-sgm-react-feature-crud_disciplina/projeto-sgm-react-feature-crud_disciplina/src/componets/pages/Instituicoes.jsx";
+import NovaInstituicao from "../../../../../Downloads/projeto-sgm-react-feature-crud_disciplina/projeto-sgm-react-feature-crud_disciplina/src/componets/pages/NovaInstituicao.jsx";
+import EditarInstituicao from "../../../../../Downloads/projeto-sgm-react-feature-crud_disciplina/projeto-sgm-react-feature-crud_disciplina/src/componets/pages/EditarInstituicao.jsx";
+import Disciplinas from "../../../../../Downloads/projeto-sgm-react-feature-crud_disciplina/projeto-sgm-react-feature-crud_disciplina/src/componets/pages/Disciplinas.jsx";
+import NovaDisciplina from "../../../../../Downloads/projeto-sgm-react-feature-crud_disciplina/projeto-sgm-react-feature-crud_disciplina/src/componets/pages/NovaDisciplina.jsx";
+import EditarDisciplina from "../../../../../Downloads/projeto-sgm-react-feature-crud_disciplina/projeto-sgm-react-feature-crud_disciplina/src/componets/pages/EditarDisciplina.jsx";
+
+// NOVOS IMPORTS PARA O CRUD DE COORDENADORES
+import Coordenadores from "../pages/Coordenadores.jsx";
+import NovoCoordenador from "../pages/NovoCoordenador.jsx";
 
 function AppRoutes() {
-  const location = useLocation();
+    const location = useLocation();
 
-  // rotas que não devem exibir a NavBar
-  const rotasSemNav = ["/", "/senhaEsquecida", "/cadastrarAluno"];
-  const mostrarCon = !rotasSemNav.includes(location.pathname);
+    // rotas que não devem exibir a NavBar
+    const rotasSemNav = ["/", "/senhaEsquecida", "/cadastrarAluno"];
+    const mostrarCon = !rotasSemNav.includes(location.pathname);
 
-  return (
-    <>
-      {mostrarCon && <Cabeca />}
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/senhaEsquecida" element={<SenhaEsquecida />} />
+    return (
+        <>
+            {mostrarCon && <Cabeca />}
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/senhaEsquecida" element={<SenhaEsquecida />} />
 
-        <Route
-          path="/aluno/:id"
-          element={
-            <RotaProtegida
-              perfilPermitido={[
-                "professor",
-                "coordenador",
-                "aluno",
-                "monitor",
-                "admin",
-              ]}
-            >
-              <Aluno />
-            </RotaProtegida>
-          }
-        />
+                <Route
+                    path="/aluno/:id"
+                    element={
+                        <RotaProtegida
+                            perfilPermitido={[
+                                "professor",
+                                "coordenador",
+                                "aluno",
+                                "monitor",
+                                "admin",
+                            ]}
+                        >
+                            <Aluno />
+                        </RotaProtegida>
+                    }
+                />
 
-        <Route
-          path="/perfil/:id"
-          element={
-            <RotaProtegida
-              perfilPermitido={[
-                "professor",
-                "coordenador",
-                "aluno",
-                "monitor",
-                "admin",
-              ]}
-            >
-              <Perfil />
-            </RotaProtegida>
-          }
-        />
+                <Route
+                    path="/perfil/:id"
+                    element={
+                        <RotaProtegida
+                            perfilPermitido={[
+                                "professor",
+                                "coordenador",
+                                "aluno",
+                                "monitor",
+                                "admin",
+                            ]}
+                        >
+                            <Perfil />
+                        </RotaProtegida>
+                    }
+                />
 
-        <Route
-          path="/editais"
-          element={
-            <RotaProtegida
-              perfilPermitido={[
-                "professor",
-                "coordenador",
-                "aluno",
-                "monitor",
-                "admin",
-              ]}
-            >
-              <Editais />
-            </RotaProtegida>
-          }
-        />
+                <Route
+                    path="/editais"
+                    element={
+                        <RotaProtegida
+                            perfilPermitido={[
+                                "professor",
+                                "coordenador",
+                                "aluno",
+                                "monitor",
+                                "admin",
+                            ]}
+                        >
+                            <Editais />
+                        </RotaProtegida>
+                    }
+                />
 
-        <Route
-          path="/monitorias"
-          element={
-            <RotaProtegida
-              perfilPermitido={[
-                "professor",
-                "coordenador",
-                "aluno",
-                "monitor",
-                "admin",
-              ]}
-            >
-              <Monitorias />
-            </RotaProtegida>
-          }
-        />
+                <Route
+                    path="/monitorias"
+                    element={
+                        <RotaProtegida
+                            perfilPermitido={[
+                                "professor",
+                                "coordenador",
+                                "aluno",
+                                "monitor",
+                                "admin",
+                            ]}
+                        >
+                            <Monitorias />
+                        </RotaProtegida>
+                    }
+                />
 
-        <Route
-          path="/alunos"
-          element={
-            <RotaProtegida perfilPermitido={["coordenador", "admin"]}>
-              <Alunos />
-            </RotaProtegida>
-          }
-        />
+                <Route
+                    path="/alunos"
+                    element={
+                        <RotaProtegida perfilPermitido={["coordenador", "admin"]}>
+                            <Alunos />
+                        </RotaProtegida>
+                    }
+                />
 
-        <Route
-          path="/professor"
-          element={
-            <RotaProtegida perfilPermitido={["professor", "coordenador"]}>
-              <Professor />
-            </RotaProtegida>
-          }
-        />
+                <Route
+                    path="/professor"
+                    element={
+                        <RotaProtegida perfilPermitido={["professor", "coordenador"]}>
+                            <Professor />
+                        </RotaProtegida>
+                    }
+                />
 
-        <Route path="/cadastrarAluno" element={<CadastrarAluno />} />
+                <Route path="/cadastrarAluno" element={<CadastrarAluno />} />
 
-        <Route
-          path="/coordenador"
-          element={
-            <RotaProtegida perfilPermitido="coordenador">
-              <Coordenador />
-            </RotaProtegida>
-          }
-        />
+                <Route
+                    path="/coordenador"
+                    element={
+                        <RotaProtegida perfilPermitido="coordenador">
+                            <Coordenador />
+                        </RotaProtegida>
+                    }
+                />
 
-        <Route
-          path="/monitor"
-          element={
-            <RotaProtegida perfilPermitido="monitor">
-              <Monitor />
-            </RotaProtegida>
-          }
-        />
+                <Route
+                    path="/monitor"
+                    element={
+                        <RotaProtegida perfilPermitido="monitor">
+                            <Monitor />
+                        </RotaProtegida>
+                    }
+                />
 
-        <Route
-          path="/admin"
-          element={
-            <RotaProtegida perfilPermitido="admin">
-              <Admin />
-            </RotaProtegida>
-          }
-        />
-        <Route path="/instituicoes" element={<Instituicoes />} />
+                <Route
+                    path="/admin"
+                    element={
+                        <RotaProtegida perfilPermitido="admin">
+                            <Admin />
+                        </RotaProtegida>
+                    }
+                />
 
-        <Route path="/instituicoes/novo" element={<NovaInstituicao />} />
+                {/* ROTAS ADICIONADAS PARA GERENCIAR COORDENADORES */}
+                <Route
+                    path="/coordenadores"
+                    element={
+                        <RotaProtegida perfilPermitido="admin">
+                            <Coordenadores />
+                        </RotaProtegida>
+                    }
+                />
+                <Route
+                    path="/coordenadores/novo"
+                    element={
+                        <RotaProtegida perfilPermitido="admin">
+                            <NovoCoordenador />
+                        </RotaProtegida>
+                    }
+                />
 
-        <Route path="/instituicoes/:id" element={<EditarInstituicao />} />
+                {/* BÔNUS: Adicionei proteção às rotas de Instituição que estavam abertas */}
+                <Route
+                    path="/instituicoes"
+                    element={
+                        <RotaProtegida perfilPermitido="admin">
+                            <Instituicoes />
+                        </RotaProtegida>
+                    }
+                />
+                <Route
+                    path="/instituicoes/novo"
+                    element={
+                        <RotaProtegida perfilPermitido="admin">
+                            <NovaInstituicao />
+                        </RotaProtegida>
+                    }
+                />
+                <Route
+                    path="/instituicoes/:id"
+                    element={
+                        <RotaProtegida perfilPermitido="admin">
+                            <EditarInstituicao />
+                        </RotaProtegida>
+                    }
+                />
 
-        <Route
-          path="/disciplinas"
-          element={
-            <RotaProtegida perfilPermitido={["coordenador", "admin"]}>
-              <Disciplinas />
-            </RotaProtegida>
-          }
-        />
-        <Route
-          path="/disciplinas/novo"
-          element={
-            <RotaProtegida perfilPermitido={["coordenador", "admin"]}>
-              <NovaDisciplina />
-            </RotaProtegida>
-          }
-        />
-        <Route
-          path="/disciplinas/:id"
-          element={
-            <RotaProtegida perfilPermitido={["coordenador", "admin"]}>
-              <EditarDisciplina />
-            </RotaProtegida>
-          }
-        />
-      </Routes>
-    </>
-  );
+                <Route
+                    path="/disciplinas"
+                    element={
+                        <RotaProtegida perfilPermitido={["coordenador", "admin"]}>
+                            <Disciplinas />
+                        </RotaProtegida>
+                    }
+                />
+                <Route
+                    path="/disciplinas/novo"
+                    element={
+                        <RotaProtegida perfilPermitido={["coordenador", "admin"]}>
+                            <NovaDisciplina />
+                        </RotaProtegida>
+                    }
+                />
+                <Route
+                    path="/disciplinas/:id"
+                    element={
+                        <RotaProtegida perfilPermitido={["coordenador", "admin"]}>
+                            <EditarDisciplina />
+                        </RotaProtegida>
+                    }
+                />
+            </Routes>
+        </>
+    );
 }
 
 export default AppRoutes;
